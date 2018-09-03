c9a72f273a5f6ee994496c083d632bf6480af6b4
mcqx2000 <mcqx2000@163.com>
2018-09-03 22:12:26 +0800

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -23,14 +23,12 @@ function distinct(arr){
 //3.找出数组中重复的元素
 function findRepeat(arr){
 	var newArr=[];
+	arr.sort();
 	for(var i=0;i<arr.length;i++){
-		for(var j=i+1;j<arr.length;j++){
-			if(arr[i]===arr[j]){
-				j=++i;
+		if(arr.indexOf(arr[i])!==arr.lastIndexOf(arr[i])&&newArr.indexOf(arr[i])===-1){
+			newArr.push(arr[i]); 
 			} 
-			
-		}
-		newArr.push(arr[j]);
+		i=arr.lastIndexOf(arr[i]);
 	}
 	return newArr;
 }
